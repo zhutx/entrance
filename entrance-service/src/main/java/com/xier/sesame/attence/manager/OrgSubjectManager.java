c2dca1b59ec45231a6570fc006ad3f0b5c7b8ffb
zhutx <16530202@qq.com>
2017-09-07 13:00:54 +0800

    初始化

--- entrance-service/src/main/java/com/xier/sesame/attence/manager/OrgSubjectManager.java
+++
@@ -1,9 +0,0 @@
-package com.xier.sesame.attence.manager;
-
-import com.xier.sesame.attence.domain.OrgSubject;
-
-public interface OrgSubjectManager {
-	
-	OrgSubject getOrgSubject(Long orgId, Long subjectId);
-
-}
